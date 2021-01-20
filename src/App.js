f06b7d8926411b20166cb7d7c21e967be54b8974
dong uk <gusrn1255@naver.com>
2021-01-20 23:04:31 +0900

    문제점 작성 깃에 코멘트작성예정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    //1. 데이터 담을공간
+    const [loans, setLoans] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    //3. 네트워킹
+
+    const getData = async () => {
+        return (
+            await axios.get('https://api.kivaws.org/v1/loans/newest.json')
+                // .then(res => console.log(res.data.loans))
+                .then(res => {
+                    setLoans(res.data.loans)
+                    setLoading(false)
+                })
+                .catch(err => console.log(err))
+        )
+    }
+
+
+    //2. 자동실행 함수
+    useEffect(() => {
+        getData()
+    }, [])
+
+    //화면에 보여주는 부분 로딩화면 포함
+    return (
+        <>
+            {loading ?
+                <div>
+                    <h1>
+                        loading ....
+                    </h1>
+                </div>
+                : (
+                    <div>
+                        {loans.map(loan => (
+                            <>
+                                <h1>Name : {loan.name}</h1>
+                                <h2>Nation : {loan.location.country}</h2>
+                                <h3>Amount : {loan.loan_amount}$</h3>
+
+                            </>
+                        ))}
+                    </div>
+                )
+            }
+        </>
+    )
 }
+
+
+
+
 
 export default App;
